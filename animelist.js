6fd3642afbaf9c7dd77ced30a73741ae3e1dfc6a
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-10-12 14:31:14 +0100

    Create animelist.js

---
+++ animelist.js
@@ -0,0 +1,8 @@
+const collection = document.getElementsByClassName("h2_anime_title")
+ const outerHTMLarr= []
+ for (let i=0; i<collection.length; i++ ){
+ console.log(collection[i].outerText)
+ }
+
+//this one just gets the list of anime on the seasonal animelist on Myanimelist.net
+// its to be run in the browser, but if you can find a better way to use it feel free
